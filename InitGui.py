58dc6513b961a333fa095a22f6ecf03eae1952df
Philip Z <philip.zimmermann@tum.de>
2021-08-03 16:40:41 +0200

    refactoring

--- InitGui.py
+++ InitGui.py
@@ -4,52 +4,56 @@ __author__ = "Johannes Hechtl"
 __email__ = "johannes.hechtl@tum.de"
 __version__ = "1.0"
 
+import FreeCAD, FreeCADGui
 
-import FreeCAD, FreeCADGui 
 
-class ScriptCmd: 
+class ScriptCmd:
     """Example Class for a simple command."""
     """For every in the GUI selectable command a class has to be written, with at least the two following functions:"""
 
-    def Activated(self): 
+    def Activated(self):
         """ This code gets executed when the corresponding button is pressed."""
-       
+
         FreeCAD.Console.PrintMessage('Hello, World!')
 
-    def GetResources(self): 
+    def GetResources(self):
         """ Return resources for GUI"""
-        return {'Pixmap' : 'path_to_an_icon/myicon.png', 'MenuText': 'Short text', 'ToolTip': 'Hello World button.'} 
+        return {'Pixmap': 'path_to_an_icon/myicon.png', 'MenuText': 'Short text', 'ToolTip': 'Hello World button.'}
         # Pixmap is a png icon that is the face of the button.
         # The tooltip gets displayed when hovering over the button
-       
+
+
 # Registering the command with the GUI. This needs to be done before the Workbench is initialized
 FreeCADGui.addCommand('Script_Cmd', ScriptCmd())
 
 
-
-class OSM_Buildings (Workbench):
-
+class OSM_Buildings(Workbench):
     MenuText = "OSM-Buildings"
     ToolTip = "A description of my workbench"
-    Icon = "paste here the contents of a 16x16 xpm icon"
+    resource_path = FreeCAD.getHomePath() + "Mod/OSM-Buildings/resources/"
+    import os
+    if not os.path.exists(resource_path):
+        resource_path = FreeCAD.getUserAppDataDir() + "Mod/OSM-Buildings/resources/"
+    Icon = resource_path + "house.svg"
 
     def Initialize(self):
         """This function is executed when FreeCAD starts"""
-        #import MyModuleA, MyModuleB # import here all the needed files that create your FreeCAD commands
-        
+        # import MyModuleA, MyModuleB # import here all the needed files that create your FreeCAD commands
+
         # if you have command classes in other files, they need to be imported EXACTLY here
 
         import my_test
-        import main_script
-        import script_new_fassade
+        import osm_to_3d_model
+        import place_facade
+        import place_image
 
-        self.list = ['Script_Cmd', 'My_Command', 'MainCommand', 'NewFassade'] # A list of the command names created
+        self.list = ['OSMtoCAD', 'PlaceFacade',
+                     'PlaceImage']  # A list of the command names created
         # commands, that are not listed here, will not show up in the GUI
 
-
-        self.appendToolbar("My Commands",self.list) # creates a new toolbar with your commands
-        self.appendMenu("My New Menu",self.list) # creates a new menu
-        self.appendMenu(["An existing Menu","My submenu"],self.list) # appends a submenu to an existing menu
+        self.appendToolbar("My Commands", self.list)  # creates a new toolbar with your commands
+        self.appendMenu("My New Menu", self.list)  # creates a new menu
+        self.appendMenu(["An existing Menu", "My submenu"], self.list)  # appends a submenu to an existing menu
 
     def Activated(self):
         """This function is executed when the workbench is activated"""
@@ -62,13 +66,12 @@ class OSM_Buildings (Workbench):
     def ContextMenu(self, recipient):
         """This is executed whenever the user right-clicks on screen"""
         # "recipient" will be either "view" or "tree"
-        self.appendContextMenu("My commands",self.list) # add commands to the context menu
+        self.appendContextMenu("My commands", self.list)  # add commands to the context menu
 
-    def GetClassName(self): 
+    def GetClassName(self):
         # This function is mandatory if this is a full python workbench
         # This is not a template, the returned string should be exactly "Gui::PythonWorkbench"
         return "Gui::PythonWorkbench"
-       
 
 
 Gui.addWorkbench(OSM_Buildings())
---
+++ osm_to_3d_model.py
@@ -0,0 +1,52 @@
+__author__ = "Johannes Hechtl"
+__email__ = "johannes.hechtl@tum.de"
+__version__ = "1.0"
+
+
+
+from BuildingObject import makeBuilding
+import os
+import FreeCAD, FreeCADGui
+import Part, PartGui 
+from PySide import QtGui
+import xml.etree.ElementTree as ET
+from Map import Map
+import cv2 as cv
+
+
+class OSMtoCAD():
+    """This command loads a .osm file and adds 3d models of all buildings to the currently active workspace"""
+
+    def GetResources(self):
+        resource_path = FreeCAD.getHomePath() + "Mod/OSM-Buildings/resources/"
+        if not os.path.exists(resource_path):
+            resource_path = FreeCAD.getUserAppDataDir() + "Mod/OSM-Buildings/resources/"
+        return {'Pixmap'  : resource_path + "map.svg", # the name of a svg file available in the resources
+                'Accel' : "Shift+S", # a default shortcut (optional)
+                'MenuText': "OSM to CAD",
+                'ToolTip' : "Load .osm file"}
+
+    def Activated(self):
+        
+        doc=FreeCAD.activeDocument() 
+        
+        filename = QtGui.QFileDialog().getOpenFileName()[0]
+
+        map = Map(filename)
+
+        doc=FreeCAD.activeDocument() 
+        
+        # add buildings to FreeCad Document
+        for building in map.buildings:
+            makeBuilding(building)
+
+
+        doc.recompute()
+        return
+
+    def IsActive(self):
+        """Here you can define if the command must be active or not (greyed) if certain conditions
+        are met or not. This function is optional."""
+        return True
+
+FreeCADGui.addCommand('OSMtoCAD', OSMtoCAD())
